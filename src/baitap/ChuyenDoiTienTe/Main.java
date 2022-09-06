afab3b9e55bcc8508f6bfca865ff7e04f1423b86
sangpk <dinhsangcnc@gmail.com>
2022-09-06 16:02:59 +0700

    modun2_java

--- src/baitap/ChuyenDoiTienTe/Main.java
+++ src/baitap/ChuyenDoiTienTe/Main.java
@@ -1,4 +1,15 @@
 package baitap.ChuyenDoiTienTe;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        double vnd=23000;
+        double usd;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("mời bạn nhập số tiền: ");
+        usd= sc.nextDouble();
+        double DoiTien=usd*23000;
+        System.out.print("giá trị của vnd: " + DoiTien);
+    }
 }
